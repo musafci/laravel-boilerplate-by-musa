6f5104c086577d2e03fd59615ea7565dfad5f8cb
Md Abu Musa <musa.fci@gmail.com>
2024-01-22 23:57:01 +0600

    Admin profile display & change password functionality

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class IndexController extends Controller
 {
@@ -11,8 +16,51 @@ class IndexController extends Controller
         return view('index.dashboard');
     }
 
-    public function profile()
+    /**
+     * @param Request $request
+     * @return Application|Factory|View
+     */
+    public function profile(Request $request): View|Factory|Application
     {
-        dd("profile");
+        $breadcrumbs = [
+            'Profile',
+        ];
+        $tab = $request->input('tab') ?? null;
+        $id = Auth::user()->id;
+        $user = User::where('id', $id)->with('roles')->get()->toArray();
+        if (!empty($user)) {
+            $user = array_shift($user);
+        }
+
+        return view('index.profile_view', compact('breadcrumbs', 'tab', 'user'));
+    }
+
+    /**
+     * @param Request $request
+     * @return RedirectResponse
+     */
+    public function storeChangePassword(Request $request): RedirectResponse
+    {
+        $validator = Validator::make($request->all(), [
+            'current_password' => 'required',
+            'password' => 'required|string|min:6|confirmed',
+            'password_confirmation' => 'required',
+        ]);
+        if ($validator->fails()) {
+            return  redirect('/profile?tab=password')->withInput()->withErrors($validator->errors());
+        }
+
+        $user = Auth::user();
+        if (!Hash::check($request->current_password, $user->password)) {
+            return redirect('/profile?tab=password')->withInput()->withErrors(['current_password' => 'Current password does not match!']);
+        }
+        $user->password = bcrypt($request->password);
+        $user->save();
+        $notification = array(
+            'message' => 'You have successfully changed your password.',
+            'alert-type' => 'success'
+        );
+
+        return back()->with($notification);
     }
 }
