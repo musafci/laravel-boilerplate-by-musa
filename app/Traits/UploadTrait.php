ffe0c1908dbe7a96e2a73584dc0a2f50bd1d515b
Md Abu Musa <musa.fci@gmail.com>
2024-02-03 23:42:07 +0600

    Update image upload functionality and install hashids package

--- app/Traits/UploadTrait.php
+++ app/Traits/UploadTrait.php
@@ -2,61 +2,60 @@
 
 namespace App\Traits;
 use Illuminate\Support\Facades\File;
-use Intervention\Image\ImageManagerStatic as Image;
+use Intervention\Image\ImageManager as Image;
 
 trait UploadTrait {
-    public function uploadBase64ToLocal($base64Image, $imagePath, $prefix, $previousImage = null) {
-        // Get the extension from the base64 image data
-        $imageParts = explode(";base64,", $base64Image);
-        $imageTypeAux = explode("image/", $imageParts[0]);
-        $imageType = $imageTypeAux[1];
-        $image = base64_decode($imageParts[1]);
+    // public function uploadBase64ToLocalOFF($base64Image, $imagePath, $prefix, $previousImage = null) {
+    //     // Get the extension from the base64 image data
+    //     $imageParts = explode(";base64,", $base64Image);
+    //     $imageTypeAux = explode("image/", $imageParts[0]);
+    //     $imageType = $imageTypeAux[1];
+    //     $image = base64_decode($imageParts[1]);
 
-        // Generate a unique filename for the image
-        $filename = $prefix . time() . '.' . $imageType;
+    //     // Generate a unique filename for the image
+    //     $filename = $prefix . time() . '.' . $imageType;
 
-        $finalDestinationPath = 'app/public/' . $imagePath;
-        self::checkDirectory($finalDestinationPath);
-        $imageResize = Image::make($image);
-        $imageResize->save(storage_path("{$finalDestinationPath}/{$filename}"));
-        $imageUrl = '/storage'.$imagePath. '/' . $filename;
-        return $imageUrl;
+    //     // Specify the path where you want to store the uploaded image
+    //     $path = public_path($imagePath . $filename);
 
-        // Delete the previous image if it exists
-        // if ($imagePath.$previousImage && File::exists($imagePath.$previousImage)) {
-        //     File::delete($imagePath.$previousImage);
-        // }
-        // return $filename;
-    }
-    public function uploadImageToLocal($image, $imagePath, $prefix = null, $previousImage = null) {
-        $filename = $prefix . generateUUID(time(),12) . '.' . $image->getClientOriginalExtension();
-        $finalDestinationPath = 'app/public/' . $imagePath;
-        self::checkDirectory($finalDestinationPath);
-        $imageResize = Image::make($image);
-        $imageResize->save(storage_path("{$finalDestinationPath}/{$filename}"));
-        $imageUrl = '/storage'.$imagePath. '/' . $filename;
-        return $imageUrl;
+    //     // // Save the image to the specified path
+    //     // file_put_contents($path, $image);
 
-        // Delete the previous image if it exists
-        // if ($imagePath.$previousImage && File::exists($imagePath.$previousImage)) {
-        //     File::delete($imagePath.$previousImage);
-        // }
-        // return $filename;
-    }
-    public function uploadBase64ToLocalOLD($image, $destinationPath, $width = null, $height = null, $previousImage = null)
+    //     // Save the original image
+    //     $image = Image::make($image);
+    //     $image->save($path);
+
+    //     // Delete the previous image if it exists
+    //     if ($imagePath.$previousImage && File::exists($imagePath.$previousImage)) {
+    //         File::delete($imagePath.$previousImage);
+    //     }
+    //     return $filename;
+    // }
+
+    // public function uploadImageToLocalOFF($image, $imagePath, $prefix, $previousImage = null) {
+    //     $filename = $prefix . time() . '.' . $image->getClientOriginalExtension();
+    //     $path = public_path($imagePath . $filename);
+
+    //     // Save the original image
+    //     $image = Image::make($image);
+    //     $image->save($path);
+
+    //     // Delete the previous image if it exists
+    //     if ($imagePath.$previousImage && File::exists($imagePath.$previousImage)) {
+    //         File::delete($imagePath.$previousImage);
+    //     }
+    //     return $filename;
+    // }
+
+    public function uploadImageToLocal($image, $destinationPath, $prefix, $width = null, $height = null, $previousImage = null)
     {
         try {
-            $image_parts = explode(";base64,", $image);
-            $image_type_aux = explode("image/", $image_parts[0]);
-            $ext = $image_type_aux[1];
-
-            $image = str_replace('data:image/'.$ext.';base64,', '', $image);
-            $file  = str_replace(' ', '+', $image);
             $finalDestinationPath = 'app/public/' . $destinationPath;
-             self::checkDirectory($finalDestinationPath);
 
-            $imageResize = Image::make(base64_decode($file));
-            $name = rand(100000, 999999) . time() . '.' . $ext;
+            self::checkDirectory($finalDestinationPath);
+
+            $imageResize = Image::make($image);
+            $name = $prefix . rand(100000, 999999) . time() . '.' . $image->getClientOriginalExtension();
 
             if (!empty($width) && !empty($height)) {
 
@@ -75,14 +74,64 @@ trait UploadTrait {
             }
 
             $imageResize->save(storage_path("{$finalDestinationPath}/{$name}"));
-            $imageUrl = '/storage'.$destinationPath. '/' . $name;
+            $imageUrl = $destinationPath . $name;
 
-            // if (!empty($previousImage)) {
-            //     $this->deleteImage($previousImage, $finalDestinationPath);
-            // }
+            if (!empty($previousImage)) {
+                $this->deleteImage($previousImage, $finalDestinationPath);
+            }
+
             return $imageUrl;
+
         } catch (\Exception $ex) {
-            Log::info('image upload ex = '. $ex->getMessage());
+            \Log::info('Image upload exception = '. $ex->getMessage());
+            return false;
+        }
+    }
+
+    public function uploadBase64ToLocal($image, $destinationPath, $prefix, $width = null, $height = null, $previousImage = null)
+    {
+        try {
+            $image_parts = explode(";base64,", $image);
+            $image_type_aux = explode("image/", $image_parts[0]);
+            $ext = $image_type_aux[1];
+
+            $image = str_replace('data:image/'.$ext.';base64,', '', $image);
+            $file  = str_replace(' ', '+', $image);
+
+            $finalDestinationPath = 'app/public/' . $destinationPath;
+
+            self::checkDirectory($finalDestinationPath);
+
+            $imageResize = Image::make(base64_decode($file));
+            $name = $prefix . rand(100000, 999999) . time() . '.' . $ext;
+
+            if (!empty($width) && !empty($height)) {
+
+                $orgWidth  = $imageResize->width();
+                $orgHeight = $imageResize->height();
+
+                if ($orgWidth >= $orgHeight) {
+                    $imageResize->resize($width, null, function ($constraint) {
+                        $constraint->aspectRatio();
+                    });
+                } else {
+                    $imageResize->resize(null, $height, function ($constraint) {
+                        $constraint->aspectRatio();
+                    });
+                }
+            }
+
+            $imageResize->save(storage_path("{$finalDestinationPath}/{$name}"));
+            $imageUrl = $destinationPath . $name;
+
+            if (!empty($previousImage)) {
+                $this->deleteImage($previousImage, $finalDestinationPath);
+            }
+
+            return $imageUrl;
+
+        } catch (\Exception $ex) {
+            \Log::info('Image upload exception = '. $ex->getMessage());
             return false;
         }
     }
@@ -95,8 +144,8 @@ trait UploadTrait {
     public function deleteImage($imagePath, $directory)
     {
         $file_name  = substr($imagePath, strrpos($imagePath, '/') + 1);
-        $path       = $directory . $file_name;
-        File::delete($path);
+        $path = $directory . $file_name;
+        File::delete(storage_path($path));
         return true;
     }
 }
