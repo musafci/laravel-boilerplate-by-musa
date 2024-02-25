b334036302be089a6958b5e1a76d9b22bec55daf
Md Abu Musa <musa.fci@gmail.com>
2024-02-25 22:49:36 +0600

    Start working with blog functionality.

---
+++ database/migrations/2024_02_26_163059_create_blogs_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')->references('id')->on('categories');
+            $table->string('title');
+            $table->text('body');
+            $table->string('image');
+            $table->integer('created_by')->nullable()->default(0);
+            $table->integer('updated_by')->nullable()->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('blogs');
+    }
+};
